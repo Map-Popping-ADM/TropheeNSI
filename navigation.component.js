9ece4e6aaeece252b0927a0afad4d54c78c349ce
Lea Lamzani <leo.cholvy@gmail.com>
2022-03-29 17:44:51 +0200

    navi

--- navigation.component.js
+++ navigation.component.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { HomeScreen } from './home.component';
-import { DetailsScreen } from './details.component';
+import { HomeScreen } from './components/home.component';
+import { DetailsScreen } from './components/details.component';
 
 const { Navigator, Screen } = createStackNavigator();
 
